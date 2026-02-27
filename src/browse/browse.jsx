c2e6b692e39c4108d4c5f1de31dfe6e0f281743c
bennethill <bennethill99@gmail.com>
2026-02-26 19:12:55 -0700

    Viewable details in browse page available now

--- src/browse/browse.jsx
+++ src/browse/browse.jsx
@@ -58,7 +58,7 @@ export function Browse() {
                             </div>
                             <div className="card-body">
                                 <p className="card-text"><strong>Username:</strong> {mech.username}</p>
-                                
+
                                 <table className="table table-sm table-bordered mt-3">
                                     <tbody>
                                         <tr>
@@ -85,7 +85,7 @@ export function Browse() {
                                 </table>
                             </div>
                             <div className="card-footer">
-                                <button 
+                                <button
                                     className="btn btn-outline-primary btn-sm w-100"
                                     onClick={() => setSelectedMech(mech)}
                                 >
@@ -96,6 +96,104 @@ export function Browse() {
                     </div>
                 ))}
             </div>
+
+            {/* Modal - only shows when selectedMech is not null */}
+            {selectedMech && (
+                <div className="modal show d-block" style={{ backgroundColor: 'rgba(0,0,0,0.5)' }}>
+                    <div className="modal-dialog modal-lg modal-dialog-centered">
+                        <div className="modal-content">
+                            <div className="modal-header bg-primary text-white">
+                                <h5 className="modal-title">{selectedMech.name}</h5>
+                                <button 
+                                    type="button" 
+                                    className="btn-close btn-close-white" 
+                                    onClick={() => setSelectedMech(null)}
+                                ></button>
+                            </div>
+                            <div className="modal-body">
+                                <p><strong>Created by:</strong> {selectedMech.username}</p>
+                                <hr />
+                                
+                                <div className="row">
+                                    <div className="col-md-6">
+                                        <h6>Parts Configuration</h6>
+                                        <ul className="list-group list-group-flush">
+                                            <li className="list-group-item d-flex justify-content-between">
+                                                <span>Head:</span>
+                                                <span>{selectedMech.parts.head}</span>
+                                            </li>
+                                            <li className="list-group-item d-flex justify-content-between">
+                                                <span>Left Shoulder:</span>
+                                                <span>{selectedMech.parts.leftShoulder}</span>
+                                            </li>
+                                            <li className="list-group-item d-flex justify-content-between">
+                                                <span>Right Shoulder:</span>
+                                                <span>{selectedMech.parts.rightShoulder}</span>
+                                            </li>
+                                            <li className="list-group-item d-flex justify-content-between">
+                                                <span>Left Hand:</span>
+                                                <span>{selectedMech.parts.leftHand}</span>
+                                            </li>
+                                            <li className="list-group-item d-flex justify-content-between">
+                                                <span>Right Hand:</span>
+                                                <span>{selectedMech.parts.rightHand}</span>
+                                            </li>
+                                            <li className="list-group-item d-flex justify-content-between">
+                                                <span>Body:</span>
+                                                <span>{selectedMech.parts.body}</span>
+                                            </li>
+                                            <li className="list-group-item d-flex justify-content-between">
+                                                <span>Core:</span>
+                                                <span>{selectedMech.parts.core}</span>
+                                            </li>
+                                            <li className="list-group-item d-flex justify-content-between">
+                                                <span>Legs:</span>
+                                                <span>{selectedMech.parts.legs}</span>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div className="col-md-6">
+                                        <h6>Statistics</h6>
+                                        <table className="table table-bordered">
+                                            <tbody>
+                                                <tr>
+                                                    <td><strong>Attack</strong></td>
+                                                    <td>{selectedMech.stats.attack}</td>
+                                                </tr>
+                                                <tr>
+                                                    <td><strong>Armor</strong></td>
+                                                    <td>{selectedMech.stats.armor}</td>
+                                                </tr>
+                                                <tr>
+                                                    <td><strong>Speed</strong></td>
+                                                    <td>{selectedMech.stats.speed}</td>
+                                                </tr>
+                                                <tr>
+                                                    <td><strong>Weight</strong></td>
+                                                    <td>{selectedMech.stats.weight}</td>
+                                                </tr>
+                                                <tr>
+                                                    <td><strong>Energy</strong></td>
+                                                    <td>{selectedMech.stats.energy}</td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="modal-footer">
+                                <button 
+                                    type="button" 
+                                    className="btn btn-secondary" 
+                                    onClick={() => setSelectedMech(null)}
+                                >
+                                    Close
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
         </main>
     )
 }
